2581710aaebefd3b22337ca7e39d9cc495f092be
kiranks593878 <69397168+kiranks593878@users.noreply.github.com>
2020-12-07 15:16:24 +0530

    created  pythoncodes.py
    This file prints the area of circle and file extension on screen

---
+++ pythoncodes.py
@@ -0,0 +1,9 @@
+code for area of circle ?
+from math import pi
+r=float(input("Enter the radius of circle:"))
+print("The area of the circle with radius " + str(r) + " is :" + str(pi*r**2))
+
+code for file extension ?
+filename= input("input the Filename:")
+F_extns= filename.split(".")
+print("The extension of the file is:" +repr(F_extns[-1]))
